f38604a2e43e610ead1bc2b835686cd4c7b20d2c
FacuLavtech <facuhlavagninodq@gmail.com>
2020-11-11 04:16:25 -0300

    change readme

--- src/components/Api/Api.jsx
+++ src/components/Api/Api.jsx
@@ -1,13 +1,59 @@
-import React, {Fragment} from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import '../Page/page.scss'
 import './api.scss'
 
 export default function Api() {
+
+    const [link, setLink] = useState({
+        url: ''
+    })
+
+    const [linkToShort, setLinkToShort] = useState('')
+
+    let { url } = link
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setLinkToShort(url)
+
+        setLink({
+            url: ''
+        })
+    }
+
+    const handleChange = (e) => {
+        setLink({
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+
+    useEffect(() =>{
+
+        const apiCall = () =>{
+
+        }
+
+    },[linkToShort])
+
+
     return (
         <Fragment>
-            <form>
-                <input type="text" placeholder="Shorten a link here..."/>
-                <input class="GetStartedBtn btnShorten" type="submit" value="Shorten It"/>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Shorten a link here..."
+                    name="url"
+                    value={url}
+                    onChange={handleChange}
+                />
+                <input
+                    className="GetStartedBtn btnShorten"
+                    type="submit"
+                    value="Shorten It"
+                />
             </form>
         </Fragment>
     )
